8acabbddcb1d77f36e20cdfb88f0085884786870
jesus callejas <david.callejasc@hotmail.es>
2020-06-09 18:22:19 -0500

    validacion de ingreso de cantida y estructura bilingue ok

--- app/src/main/java/com/example/manillas/MainActivity.java
+++ app/src/main/java/com/example/manillas/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private Spinner SpnMoneda;
     private TextView valorUnidad;
     private TextView valorPagar;
+    private TextView txtDivisa;
     private EditText cantidad;
 
     @Override
@@ -29,16 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
         valorUnidad=(TextView)findViewById(R.id.lblValorUnidad);
         valorPagar=(TextView) findViewById(R.id.lblvalorPagar);
+        txtDivisa=(TextView) findViewById(R.id.lblMonedaDetalle);
         cantidad=(EditText)findViewById(R.id.txtCantidad);
         SpnMaterial=(Spinner)findViewById(R.id.spinnerMaterial);
         SpnDije=(Spinner)findViewById(R.id.spinnerDije);
         SpnTipo=(Spinner)findViewById(R.id.spinnerTipo);
         SpnMoneda=(Spinner)findViewById(R.id.spinnerMoneda);
 
-        String [] opcMaterial={"Cuero","Cuerda"};
-        String [] opcDije={"Martillo","Ancla"};
-        String [] opcTipo={"Oro","Oro Rosado","Plata","Niquel"};
-        String [] opcMoneda={"Dolar","Peso"};
+        String [] opcMaterial={getString(R.string.materialCuero),getString(R.string.materialCuerda)};
+        String [] opcDije={getString(R.string.dijeMartillo),getString(R.string.dijeAncla)};
+        String [] opcTipo={getString(R.string.tipoOro),getString(R.string.tipoOroRosado),getString(R.string.tipoPlata),getString(R.string.tipoNiquel)};
+        String [] opcMoneda= {getString(R.string.monedaDolar), getString(R.string.monedaPeso)};
 
         ArrayAdapter<String> adapterMaterial = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,opcMaterial);
         ArrayAdapter<String> adapterDije = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,opcDije);
@@ -54,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcular(View v){
-        int cant=1,valUnidad,peso=3200;
+        int cant=1,valUnidad=0,peso=3200;
         boolean divisa=false;
         String materialSel=SpnMaterial.getSelectedItem().toString();
         String dijeSel=SpnDije.getSelectedItem().toString();
@@ -62,139 +65,127 @@ public class MainActivity extends AppCompatActivity {
         String monedaSel=SpnMoneda.getSelectedItem().toString();
 
         int valTotal;
+        if(!cantidad.getText().toString().isEmpty()) {
+            cant = Integer.parseInt(cantidad.getText().toString());
 
-        cant=Integer.parseInt(cantidad.getText().toString());
-        //---------------------------------------------------
-        if(monedaSel.equals("Dolar")){
-            divisa=true;
-        }else if(monedaSel.equals("Peso")){
-            divisa=false;
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuero")
-           &&dijeSel.equals("Martillo")
-           &&(tipoSel.equals("Oro")||tipoSel.equals("Oro Rosado"))
-        ){
-            valUnidad=100;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuero")
-                &&dijeSel.equals("Martillo")
-                &&tipoSel.equals("Plata")
-        ){
 
-            valUnidad=80;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuero")
-                &&dijeSel.equals("Martillo")
-                &&tipoSel.equals("Niquel")
-        ){
-            valUnidad=70;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuero")
-                &&dijeSel.equals("Ancla")
-                &&(tipoSel.equals("Oro")||tipoSel.equals("Oro Rosado"))
-        ){
-            valUnidad=120;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuero")
-                &&dijeSel.equals("Ancla")
-                &&tipoSel.equals("Plata")
-        ){
-            valUnidad=100;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuero")
-                &&dijeSel.equals("Ancla")
-                &&tipoSel.equals("Niquel")
-        ){
-            valUnidad=90;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuerda")
-                &&dijeSel.equals("Martillo")
-                &&(tipoSel.equals("Oro")||tipoSel.equals("Oro Rosado"))
-        ){
-            valUnidad=90;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuerda")
-                &&dijeSel.equals("Martillo")
-                &&tipoSel.equals("Plata")
-        ){
-            valUnidad=70;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuerda")
-                &&dijeSel.equals("Martillo")
-                &&tipoSel.equals("Niquel")
-        ){
-            valUnidad=50;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuerda")
-                &&dijeSel.equals("Ancla")
-                &&(tipoSel.equals("Oro")||tipoSel.equals("Oro Rosado"))
-        ){
-            valUnidad=110;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuerda")
-                &&dijeSel.equals("Ancla")
-                &&tipoSel.equals("Plata")
-        ){
-            valUnidad=90;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
-        }
-        //---------------------------------------------------
-        if(materialSel.equals("Cuerda")
-                &&dijeSel.equals("Ancla")
-                &&tipoSel.equals("Niquel")
-        ){
-            valUnidad=80;
-            valTotal=calcularTotal(valUnidad,cant);
-            valorUnidad.setText("" +valUnidad);
-            valorPagar.setText("" +valTotal);
+            //---------------------------------------------------
+            if (monedaSel.equals(getString(R.string.monedaDolar))) {
+                divisa = true;
+                txtDivisa.setText(R.string.dolaresDivisa);
+            } else if (monedaSel.equals(getString(R.string.monedaPeso))) {
+                divisa = false;
+                txtDivisa.setText(R.string.pesosDivisa);
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuero))
+                    && dijeSel.equals(getString(R.string.dijeMartillo))
+                    && (tipoSel.equals(getString(R.string.tipoOro)) || tipoSel.equals(getString(R.string.tipoOroRosado)))
+            ) {
+
+                calcularTotal(cant, 100, divisa);
+
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuero))
+                    && dijeSel.equals(getString(R.string.dijeMartillo))
+                    && tipoSel.equals(getString(R.string.tipoPlata))
+            ) {
+
+                calcularTotal(cant, 80, divisa);
+
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuero))
+                    && dijeSel.equals(getString(R.string.dijeMartillo))
+                    && tipoSel.equals(getString(R.string.tipoNiquel))
+            ) {
+                calcularTotal(cant, 70, divisa);
+
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuero))
+                    && dijeSel.equals(getString(R.string.dijeAncla))
+                    && (tipoSel.equals(getString(R.string.tipoOro)) || tipoSel.equals(getString(R.string.tipoOroRosado)))
+            ) {
+                calcularTotal(cant, 120, divisa);
+
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuero))
+                    && dijeSel.equals(getString(R.string.dijeAncla))
+                    && tipoSel.equals(getString(R.string.tipoPlata))
+            ) {
+                calcularTotal(cant, 100, divisa);
+
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuero))
+                    && dijeSel.equals(getString(R.string.dijeAncla))
+                    && tipoSel.equals(getString(R.string.tipoNiquel))
+            ) {
+                calcularTotal(cant, 90, divisa);
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuerda))
+                    && dijeSel.equals(getString(R.string.dijeMartillo))
+                    && (tipoSel.equals(getString(R.string.tipoOro)) || tipoSel.equals(getString(R.string.tipoOroRosado)))
+            ) {
+                calcularTotal(cant, 90, divisa);
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuerda))
+                    && dijeSel.equals(getString(R.string.dijeMartillo))
+                    && tipoSel.equals(getString(R.string.tipoPlata))
+            ) {
+                calcularTotal(cant, 70, divisa);
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuerda))
+                    && dijeSel.equals(getString(R.string.dijeMartillo))
+                    && tipoSel.equals(getString(R.string.tipoNiquel))
+            ) {
+                calcularTotal(cant, 50, divisa);
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuerda))
+                    && dijeSel.equals(getString(R.string.dijeAncla))
+                    && (tipoSel.equals(getString(R.string.tipoOro)) || tipoSel.equals(getString(R.string.tipoOroRosado)))
+            ) {
+                calcularTotal(cant, 110, divisa);
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuerda))
+                    && dijeSel.equals(getString(R.string.dijeAncla))
+                    && tipoSel.equals(getString(R.string.tipoPlata))
+            ) {
+                calcularTotal(cant, 90, divisa);
+            }
+            //---------------------------------------------------
+            if (materialSel.equals(getString(R.string.materialCuerda))
+                    && dijeSel.equals(getString(R.string.dijeAncla))
+                    && tipoSel.equals(getString(R.string.tipoNiquel))
+            ) {
+                calcularTotal(cant, 80, divisa);
+            }
+        }else{
+            Toast.makeText(this, R.string.ingresarCantidad,Toast.LENGTH_SHORT).show();
         }
     }
 
-    public int calcularTotal(int valorUnidad, int total){
-        return valorUnidad*total;
+    public void calcularTotal(int cant, int valUnidad, boolean divisa){
+        int val,valTotal;
+
+        if(divisa) {
+            val = valUnidad;
+        }else{
+            val=valUnidad*3200;
+        }
+        valTotal=val*cant;
+        valorUnidad.setText("" +valUnidad);
+        valorPagar.setText("" +valTotal);
+
+
     }
 
 }
